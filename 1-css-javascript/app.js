5d6c4de734b0efff2100822352cdc6e33df32231
Ariagny <ariagnyborregaleso1@gmail.com>
2024-07-11 16:05:13 -0500

    proyecto

---
+++ 1-css-javascript/app.js
@@ -0,0 +1,26 @@
+const fondo = document.querySelector('.container');
+const sol = document.querySelector('.sol');
+const nube1 = document.querySelector(".uno");
+const nube2 = document.querySelector(".dos");
+const nube3 = document.querySelector(".tres");
+const nube4 = document.querySelector(".cuatro");
+
+function noche (){
+fondo.style.backgroundColor = "blue"
+sol.style.backgroundColor = "white"
+}
+
+function moverSol (){
+sol.style.animation = "solMover 5s linear"
+}
+
+function movernub (){
+nube1.style.animation = "moverNub1 5s linear"
+nube2.style.animation = "moverNub2 5s linear"
+nube3.style.animation = "moverNub3 5s linear"
+nube4.style.animation = "moverNub4 5s linear"
+}
+
+fondo.addEventListener("click", noche);
+sol.addEventListener("click", moverSol);
+nube1.addEventListener("click", movernub);
