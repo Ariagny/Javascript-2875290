5ead283a26d8fa3f5511d4c74abbc197078c8f82
Ariagny <ariagnyborregaleso1@gmail.com>
2024-07-22 14:08:12 -0500

    proyecto

--- 3-dinamic-dom/app.js
+++ 3-dinamic-dom/app.js
@@ -1,12 +1,40 @@
 const listaColores = document.querySelector("#listaColores");
 const btnCrear = document.querySelector("#btnCrear");
 const main = document.querySelector("#main");
+const btnBorrar = document.querySelector("#btnBorrar");
+let n = 1;
 
 function crear (){
 const ball = document.createElement("div");
+const equis = document.createElement("button");
+
+equis.innerText = "X";
+equis.classList.add("equis")
+
 ball.classList.add("pelotica");
 ball.style.backgroundColor = listaColores.value
+ball.innerText = n++;
+ball.appendChild(equis);
+
 main.appendChild(ball);
-console.log(ball);
 }
-btnCrear.addEventListener("click", crear)
+
+function BorrarPantalla(){
+    main.innerHTML = "";
+    n =1;
+}
+
+function BorrarElemento(e){
+    if(e.target.innerText == "X"){
+        main.removeChild(e.target.parentElement);
+    }
+}
+
+function tacharElemento(e){
+    e.target.style.textDecorationLine = "line-through"
+}
+
+btnCrear.addEventListener("click", crear);
+btnBorrar.addEventListener("click", BorrarPantalla);
+main.addEventListener("click", BorrarElemento);
+main.addEventListener("click", tacharElemento);
