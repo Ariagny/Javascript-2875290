0e2f790203b641122be0edba82b2afe546ee0fbd
Ariagny <ariagnyborregaleso1@gmail.com>
2024-07-22 17:18:12 -0500

    proyecto

--- 4-lista-de-tareas/app.js
+++ 4-lista-de-tareas/app.js
@@ -1,12 +1,24 @@
 const input = document.querySelector("#input");
 const btn = document.querySelector(".btn");
 const main = document.querySelector("#main");
+const btnBorrar = document.querySelector(".borrar");
 
 function crear (){
 const tarea = document.createElement("div");
 tarea.classList.add("lista");
 tarea.innerText = input.value
-main.appendChild(tarea)
+main.appendChild(tarea);
+
+const quit = document.createElement("button");
+quit.innerText = `<i class="fa-regular fa-circle-xmark"></i>`;
+quit.classList.add("quit");
+tarea.appendChild(quit)
+}
+
+function BorrarPantalla(){
+    main.innerHTML = "";
+    n =1;
 }
 
 btn.addEventListener("click",crear);
+btnBorrar.addEventListener("click", BorrarPantalla);
