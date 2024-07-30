3fc4ebcd1c166f67aeabf5552b4ee7df59669f52
Ariagny <ariagnyborregaleso1@gmail.com>
2024-07-30 17:18:18 -0500

    proyecto

---
+++ 5-estructuradedatos/bd.js
@@ -0,0 +1,275 @@
+
+let Musica = [
+    {
+        id: 1,
+        imagen: 'https://i.ytimg.com/vi/cii6ruuycQA/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLAtzRzOkqh9tmiujehjmxXx8dbFRw',
+        nombre: 'Deja vu',
+        artista: 'Olivia Rodrigo',
+        genero: 'Pop',
+        año: 2021,
+        link: 'https://www.youtube.com/watch?v=cii6ruuycQA'
+    },
+    {
+        id: 2,
+        imagen: 'https://i.ytimg.com/vi/nCkpzqqog4k/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLBt_PVx7jHU9lo-zbUeNIiNZEd_Lg',
+        nombre: "I'm not the only one",
+        artista: 'Sam Smith',
+        genero: 'Pop',
+        año: 2014,
+        link: 'https://www.youtube.com/watch?v=nCkpzqqog4k'
+    },
+    {
+        id: 3,
+        imagen: 'https://i.ytimg.com/vi/1YUBbF24H44/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLB_FDT3RYF6Db3q5vW9KsL9RCIcUA',
+        nombre: 'Because i liked a boy',
+        artista: 'Sabrina Carpenter',
+        genero: 'Pop',
+        año: 2022,
+        link: 'https://www.youtube.com/watch?v=1YUBbF24H44'
+    },
+    {
+        id: 4,
+        imagen: 'https://i.ytimg.com/vi/Ahha3Cqe_fk/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLCu34-mdsK19s84FE1AB0b5O7ZJXw',
+        nombre: 'The One That Got Away',
+        artista: 'Katy Perry',
+        genero: 'Pop',
+        año: 2010,
+        link: 'https://www.youtube.com/watch?v=Ahha3Cqe_fk'
+    },
+    {
+        id: 5,
+        imagen: 'https://i.ytimg.com/vi/tcYodQoapMg/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLCLejwn2LvjU8VS4vcEOwPSfDn_Pg',
+        nombre: 'Positions',
+        artista: 'Ariana Grande',
+        genero: 'Pop',
+        año: 2020,
+        link: 'https://www.youtube.com/watch?v=tcYodQoapMg'
+    },
+    {
+        id: 6,
+        imagen: 'https://i.ytimg.com/vi/G7KNmW9a75Y/maxresdefault.jpg',
+        nombre: 'Flowers',
+        artista: 'Miley Cyrus',
+        genero: 'Pop',
+        año: 2023,
+        link: 'https://www.youtube.com/watch?v=G7KNmW9a75Y'
+    },
+    {
+        id: 7,
+        imagen: 'https://i.ytimg.com/vi/cF1Na4AIecM/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLC4roCqXYawcH8sk6JRUCUjsNDIRQ',
+        nombre: 'Please Please Please',
+        artista: 'Sabrina Carpenter',
+        genero: 'Disco-pop',
+        año: 2024,
+        link: 'https://www.youtube.com/watch?v=cF1Na4AIecM'
+    },
+    {
+        id: 8,
+        imagen: 'https://i.ytimg.com/vi/5GJWxDKyk3A/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLBKHz09QTikICj_EZKs8mYrojE31A',
+        nombre: 'Happier Than Ever',
+        artista: 'Billie Eilish',
+        genero: 'Pop rock',
+        año: 2021,
+        link: 'https://www.youtube.com/watch?v=5GJWxDKyk3A'
+    },
+    {
+        id: 9,
+        imagen: 'https://i.ytimg.com/vi/zlJDTxahav0/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLB0LmjkQhv77uwyMdldmZWqIS5D6Q',
+        nombre: 'Lose You To Love Me',
+        artista: 'Selena Gomez',
+        genero: 'Pop',
+        año: 2020,
+        link: 'https://www.youtube.com/watch?v=zlJDTxahav0'
+    },
+    {
+        id: 10,
+        imagen: 'https://i.ytimg.com/vi/ZmDBbnmKpqQ/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLB265-VNkGfGN4aBmKtebnskiNXRA',
+        nombre: 'Drivers license',
+        artista: 'Olivia Rodrigo',
+        genero: 'Indie pop',
+        año: 2021,
+        link: 'https://www.youtube.com/watch?v=ZmDBbnmKpqQ'
+    },
+    {
+        id: 11,
+        imagen: 'https://i.ytimg.com/vi/RlPNh_PBZb4/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLBgFZLFGrQWgEbiSdY_th5tJ18NmQ',
+        nombre: 'Vampire',
+        artista: 'Olivia Rodrigo',
+        genero: 'Pop rock',
+        año: 2023,
+        link: 'https://www.youtube.com/watch?v=RlPNh_PBZb4'
+    },
+    {
+        id: 12,
+        imagen: 'https://i.ytimg.com/vi/hLQl3WQQoQ0/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLAcip5yLBIgLjYL6w2dK-Y93L3xgg',
+        nombre: 'Someone Like You',
+        artista: 'Adele',
+        genero: 'Pop',
+        año: 2011,
+        link: 'https://www.youtube.com/watch?v=hLQl3WQQoQ0'
+    },
+    {
+        id: 13,
+        imagen: 'https://i.ytimg.com/vi/3tmd-ClpJxA/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLCwGyE2TdGrlyo-dw354iAitsfpPA',
+        nombre: 'Look What You Made Me Do',
+        artista: 'Taylor Swift',
+        genero: 'Electro-pop',
+        año: 2017,
+        link: 'https://www.youtube.com/watch?v=3tmd-ClpJxA'
+    },
+    {
+        id: 14,
+        imagen: 'https://i.ytimg.com/vi/fk4BbF7B29w/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLB19vuQkRszlYWs7CVsmFD1SG-FpQ',
+        nombre: 'Send My Love',
+        artista: 'Adele',
+        genero: 'Pop',
+        año: 2015,
+        link: 'https://www.youtube.com/watch?v=fk4BbF7B29w'
+    },
+    {
+        id: 15,
+        imagen: 'https://i.ytimg.com/vi/0KSOMA3QBU0/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLAA2ndt3tYm-Xet1Q6Chp1b36a34A',
+        nombre: 'Dark Horse', 
+        artista: 'Katy Perry ft. Juicy J',
+        genero: 'Pop',
+        año: 2013,
+        link: 'https://www.youtube.com/watch?v=0KSOMA3QBU0'
+    },
+    {
+        id: 16,
+        imagen: 'https://i.ytimg.com/vi/uAVUl0cAKpo/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLDpSNzXTX6ykZJOOz8gWdjo7WhwHw',
+        nombre: 'Thumbs',
+        artista: 'Sabrina Carpenter',
+        genero: 'Pop',
+        año: 2016,
+        link: 'https://www.youtube.com/watch?v=uAVUl0cAKpo'
+    },
+    {
+        id: 17,
+        imagen: 'https://i.ytimg.com/vi/pok8H_KF1FA/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLD_95XVgVZYq8siIoU1H7fSUVjb9Q',
+        nombre: 'Say So',
+        artista: 'Doja Cat',
+        genero: 'Pop',
+        año: 2019,
+        link: 'https://www.youtube.com/watch?v=pok8H_KF1FA'
+    },
+    {
+        id: 18,
+        imagen: 'https://i.ytimg.com/vi/ffxKSjUwKdU/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLANvwfhEur1a1fESErordhiQdG4bg',
+        nombre: 'No tears left to cry',
+        artista: 'Ariana Grande',
+        genero: 'Pop',
+        año: 2018,
+        link: 'https://www.youtube.com/watch?v=ffxKSjUwKdU'
+    },
+    {
+        id: 19,
+        imagen: 'https://i.ytimg.com/vi/dT2owtxkU8k/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLCqsNVTgWgj80qz7kG5DQx6p42e4w',
+        nombre: "There's Nothing Holdin' Me Back",
+        artista: 'Shawn Mendes',
+        genero: 'Pop',
+        año: 2016,
+        link: 'https://www.youtube.com/watch?v=dT2owtxkU8k'
+    },
+    {
+        id: 20,
+        imagen: 'https://i.ytimg.com/vi/fWNaR-rxAic/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLCtnMzxdzlZy9FXdbT2BYt1OiZKuw',
+        nombre: 'Call Me Maybe',
+        artista: 'Carly Rae Jepsen',
+        genero: 'Pop',
+        año: 2011,
+        link: 'https://www.youtube.com/watch?v=fWNaR-rxAic'
+    },
+    {
+        id: 21,
+        imagen: 'https://i.ytimg.com/vi/7PCkvCPvDXk/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLAGgHUKioCYe4bwQg5iFN68mE3E-g',
+        nombre: 'All About That Bass',
+        artista: 'Meghan Trainor',
+        genero: 'Dance-pop',
+        año: 2014,
+        link: 'https://www.youtube.com/watch?v=7PCkvCPvDXk'
+    },
+    {
+        id: 22,
+        imagen: 'https://i.ytimg.com/vi/E-kkX2UuBcg/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLCwssbX3hER5UaVsCCLZ6GFTBBdCA',
+        nombre: 'Besas tan bien',
+        genero: 'Regueton',
+        año: 2014,
+        artista: 'Farruko',
+        link: ''
+    },
+    {
+        id: 23,
+        imagen: 'https://i.ytimg.com/vi/AQ4TW5Z8eu0/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLB00f1UtjerIq-Td0ditZJYymQQwg',
+        nombre: 'Im still in love with',
+        genero: 'dancehall',
+        año: 2010,
+        artista: 'Sean Paul',
+        link: ''
+    },
+    {
+        id: 24,
+        imagen: 'https://i.ytimg.com/vi/L4tCfUbicfo/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLAtOeLgn2Y7TmNVJdoAQ05owHxJDA',
+        nombre: 'Hooka',
+        genero: 'Regueton',
+        año: 2017,
+        artista: 'Don omar',
+        link: ''
+    },
+    {
+        id: 25,
+        imagen: 'https://i.ytimg.com/vi/nlXqp3FVrq8/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLBZzOMze1KsEVpQGY821CwawfPmRw',
+        nombre: 'Hola',
+        genero: 'Regueton',
+        año: 2020,
+        artista: 'Dalex',
+        link: ''
+    },
+    {
+        id: 26,
+        imagen: 'https://i.ytimg.com/vi/ru7cc5jZeuU/hqdefault.jpg?s…HIgYygRMA8=&rs=AOn4CLCeBXsKzx1RZAqzFNsVLsp0rvB97A',
+        nombre: 'Miss independent',
+        genero: 'dancehall',
+        año: 2019,
+        artista: 'Eichem',
+        link: ''
+    },
+    {
+        id: 27,
+        imagen: 'https://i.ytimg.com/vi/-cdwUr6qSGo/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLAYwFj1nlFuLxtUO7tiZ9cIxjVNN',
+        nombre: 'VACAXIONES',
+        genero: 'Regueton',
+        año: 2022,
+        artista: 'Feid',
+        link: ''
+    },
+    {
+        id: 28,
+        imagen: 'https://i.ytimg.com/vi/wvz97-lNPH8/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLBAH2JosPVWb_QxebVOYQ-9A9CwWw',
+        nombre: 'BZRP Music sessions #51',
+        genero: 'Urbano',
+        año: 2022,
+        artista: 'Bizarrap',
+        link: ''
+    },
+    {
+        id: 29,
+        imagen: 'https://i.ytimg.com/vi/0OkiUUU3Odw/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLA1LVqJG3KvOhk80E4MQDICFOPeew',
+        nombre: 'BZRP Music sessions #36',
+        genero: 'Bizarrap',
+        año: 2021,
+        artista: 'Bizarrap',
+        link: ''
+    },
+    {
+        id: 30,
+        imagen: 'https://i.ytimg.com/vi/A_g3lMcWVy0/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLDXVwk_EQQB2Aqq_FW-0nGlM6dRbw',
+        nombre: 'BZRP Music sessions #52',
+        genero: 'Urbano',
+        año: 2022,
+        artista: 'Bizarrap',
+        link: ''
+    },
+   
+]
+
