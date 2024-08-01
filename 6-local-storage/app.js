f834d4980337f107e4413b35e191f475822dcf49
Ariagny <ariagnyborregaleso1@gmail.com>
2024-08-01 15:44:50 -0500

    proyecto

--- 6-local-storage/app.js
+++ 6-local-storage/app.js
@@ -51,3 +51,29 @@ borrarTodo() */
 const Username = document.querySelector("#Username");
 const Password = document.querySelector("#Password");
 const formulario = document.querySelector("#formulario");
+
+
+//inicio de sesion
+
+function validarUsuario (e){
+    e.preventDefault();
+
+    let currentUser = JSON.parse(localStorage.getItem("user"));
+    console.log(currentUser.userN);
+    console.log(currentUser.userP);
+
+console.log(Boolean(Username.value === currentUser.userN));
+console.log(Boolean(Password.value === currentUser.userP));
+
+if (Username.value === currentUser.userN && Password.value === currentUser.userP){
+    console.log("Felicidades puedes entrar 🔥")
+    window.location = "./userpage.html"
+}else {
+    console.log("Sigue intentando")
+}
+
+formulario.reset()
+}
+
+formulario.addEventListener("submit", validarUsuario)
+
