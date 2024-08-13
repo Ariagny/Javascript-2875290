9e35c34600a356633c10ff4d3d085022916cc565
Ariagny <ariagnyborregaleso1@gmail.com>
2024-08-13 15:21:21 -0500

    proyecto

--- 8-fetch-api/rickmorty.js
+++ 8-fetch-api/rickmorty.js
@@ -1,19 +1,36 @@
 const container = document.querySelector(".container")
+const  circle = document.querySelector(".circle")
+
+let e = 0
 
 let url = "https://rickandmortyapi.com/api/character"
 fetch(url)
 .then(response => response.json())
 .then(data => {
  data.results.forEach(element => {
-    const box = document.createElement('div')
-        box.classList.add("box");
-        container.appendChild(box)
+    console.log(data.results)
+    const card = document.createElement('div')
+        card.classList.add("card");
+        container.appendChild(card)
 
-        box.innerHTML = ` 
+        card.innerHTML = ` 
             <img src="${element.image}" width= "200px">
-            <p>${element.name}</p>
+            <div class="info">
+            <h1>${element.name}</h1>
+            <div class="section">
+            <div class="circle"></div>
+            <span class="estado">${element.status} - ${element.species} </span>
+            </div>
+            <div class="detalle">
+                <p><strong>Última ubicación conocida:</strong> la guarida de Worldender</p>
+                <p><strong>Visto por primera vez en:</strong> Vindicadores 3: El regreso de Worldender</p>
+            </div>
+        </div>
+        </div>
         `
-        container.appendChild(box)
+        container.appendChild(card)
  });
 }
 )
+
+function estado (e){}
