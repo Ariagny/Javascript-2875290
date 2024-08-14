0a66d2d00095b440b7578e72314ad1a42a4b5949
Ariagny <ariagnyborregaleso1@gmail.com>
2024-08-14 16:06:03 -0500

    proyecto

--- 8-fetch-api/rickmorty.js
+++ 8-fetch-api/rickmorty.js
@@ -1,13 +1,68 @@
 const container = document.querySelector(".container")
-const  circle = document.querySelector(".circle")
+const buscador = document.querySelector("#buscador")
+const textoBusqueda = document.querySelector("#textoBusqueda")
+const listaStatus = document.querySelector("#listaStatus")
+let results;
 
+
+function pintar (elemento){
+  switch(elemento){
+    case "Alive":
+      return "green"
+    break
+
+    case "Dead":
+      return "red"
+      break
+    default:
+    return "gray"
+  }
+}
+
+function filtrar (e){
+  e.preventDefault()
+
+  container.innerHTML = ""
+
+  const filtro = results.filter(word => word.status == listaStatus.value);
+  console.log(filtro)
+
+  filtro.forEach(element => {
+    const card = document.createElement('div')
+    card.classList.add("card");
+    container.appendChild(card)
+
+    card.innerHTML = ` 
+        <img src="${element.image}" width= "200px">
+        <div class="info">
+        <h1>${element.name}</h1>
+        <div class="section">
+        <div class="circle ${pintar(element.status)}"></div>
+        <span class="estado">${element.status} - ${element.species} </span>
+        </div>
+        <div class="detalle">
+            <p><strong>Última ubicación conocida:</strong><br>${element.location.name}</p>
+            <p><strong>Visto por primera vez en:</strong><br>${element.origin.name}</p>
+        </div>
+    </div>
+    </div>
+    `
+});
+
+}
+buscador.addEventListener("submit", filtrar)
 
 let url = "https://rickandmortyapi.com/api/character"
 fetch(url)
 .then(response => response.json())
 .then(data => {
+
+  results = data.results;
+  /* const results = data.results.filter(word => word.status == "Alive")
+  console.log(results)
+
+
     data.results.forEach(element => {
-        console.log(data.results)
         const card = document.createElement('div')
         card.classList.add("card");
         container.appendChild(card)
@@ -17,7 +72,7 @@ fetch(url)
             <div class="info">
             <h1>${element.name}</h1>
             <div class="section">
-            <div class="circle "${element.status === 'Alive' ? "green": "red"}></div>
+            <div class="circle ${pintar(element.status)}"></div>
             <span class="estado">${element.status} - ${element.species} </span>
             </div>
             <div class="detalle">
@@ -27,20 +82,7 @@ fetch(url)
         </div>
         </div>
         `
-        container.appendChild(card)
-
-       /*  function estado (){
-            if (element.status === "Alive"){
-              circle.style.ackground = "Green";
-            }else if (element.status === "Dead"){
-              circle.style.background = "Red";
-            }else{
-              circle.style.background = "#616161";
-            }
-          }
-
-          estado() */
- });
+ }); */
 }
 )
 
