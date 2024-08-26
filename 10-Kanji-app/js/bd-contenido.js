b0e3d3504887a4f3b260b04326c70cf9bdca87ca
Ariagny <ariagnyborregaleso1@gmail.com>
2024-08-26 15:45:09 -0500

    proyecto

---
+++ 10-Kanji-app/js/bd-contenido.js
@@ -0,0 +1,21 @@
+let Modulos = [
+    {
+        id: 1,
+        nombre: "Dibujo y bocetos",
+        video: 'https://i.ytimg.com/vi/nCkpzqqog4k/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLBt_PVx7jHU9lo-zbUeNIiNZEd_Lg',
+
+        link: 'https://www.youtube.com/watch?v=nCkpzqqog4k'
+    },
+    {
+        id: 2,
+        nombre: "Personalidad e historia",
+        video: 'https://i.ytimg.com/vi/nCkpzqqog4k/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLBt_PVx7jHU9lo-zbUeNIiNZEd_Lg',
+        link: 'https://www.youtube.com/watch?v=nCkpzqqog4k'
+    },
+    {
+        id: 2,
+        nombre: "Traje y superpoder",
+        video: 'https://i.ytimg.com/vi/nCkpzqqog4k/hqdefault.jpg?s…EIYAXABwAEG&rs=AOn4CLBt_PVx7jHU9lo-zbUeNIiNZEd_Lg',
+        link: 'https://www.youtube.com/watch?v=nCkpzqqog4k'
+    },
+]
