6d816f2702e235d72dfc12bdb1e1a5b6f2d00ad7
Ariagny <ariagnyborregaleso1@gmail.com>
2024-08-29 16:21:35 -0500

    proyecto

---
+++ 10-Kanji-app/js/index.js
@@ -0,0 +1,18 @@
+const sugerencia = document.querySelector(".sugerencia")
+const btnSugerencia = document.querySelector(".btn-sugerencia")
+const borrarSugerencia = document.querySelector(".x")
+
+function enviarSugerencia() {
+    if (sugerencia.style.display === "none") {
+      sugerencia.style.display = "block";
+    } else {
+      sugerencia.style.display = "none";
+    }
+  }
+
+function cerrarSugerencia(){
+    sugerencia.style.display = "none";
+}
+
+btnSugerencia.addEventListener("click", enviarSugerencia)
+borrarSugerencia.addEventListener("click", enviarSugerencia)
